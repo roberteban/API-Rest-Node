4ad0c602089e970a791fd9466744dd48b181c469
Roberto Castillo <38573736+roberteban@users.noreply.github.com>
2024-12-08 04:06:57 -0300

    Update article.js

--- routes/article.js
+++ routes/article.js
@@ -1,67 +1,24 @@
-const express = require("express"); 
-const multer = require("multer"); 
+const express = require("express");
+const multer = require("multer");
 const articleController = require("../controllers/article");
 const router = express.Router();
 
+// Configuración de almacenamiento para imágenes
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, './images/articles/')
-    },
-    filename: function(req, file, cb){
-        cb(null, "article"+ Date.now() + file.originalname);
-    },
-}) 
+    destination: (req, file, cb) => cb(null, "./images/articles/"),
+    filename: (req, file, cb) => cb(null, "article_" + Date.now() + path.extname(file.originalname)),
+});
+const uploads = multer({ storage });
 
-const uploads = multer({storage: storage});
-
-// Ruta de prueba
-router.get("/ruta-de-prueba", articleController.test);
+// Rutas
+router.get("/prueba", articleController.test);
 router.get("/curso", articleController.course);
-
-// Ruta útil
 router.post("/crear", articleController.postArticle);
-// Obtener todos los ultimos artículos
 router.get("/articulos/:ultimos?", articleController.listArticles);
 router.get("/articulo/:id", articleController.getArticle);
 router.delete("/articulo/:id", articleController.deleteArticle);
 router.put("/articulo/:id", articleController.putArticle);
-router.post("/subir-imagen/:id", [uploads.single("file0")], articleController.uploadImage);
-router.get("/imagen/:fichero", articleController.image);
+router.post("/subir-imagen/:id", uploads.single("file0"), articleController.uploadImage);
 router.get("/buscar/:busqueda", articleController.seeker);
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
